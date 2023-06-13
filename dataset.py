242c09d428c262c29a5c3aae94ad7de4fce255f3
Enot3811 <enot3811@gmail.com>
2023-06-13 12:30:30 +0300

    Исправлен формат docstrings

--- dataset.py
+++ dataset.py
@@ -6,44 +6,47 @@ import torch
 
 
 class ContrastiveTransformations:
-    """
-    Обработчик для contrastive обучения.
+    """Обработчик для contrastive обучения.
 
     Производит аугментацию одного изображения несколькими разными способами.
-    """    
+    """
     
     def __init__(self, base_transforms: Callable, n_views: int = 2):
-        """
-        Инициализация обработчика.
+        """Инициализация обработчика.
 
-        Args:
-            base_transforms (Callable): Трансформации для изображений.
-            n_views (int, optional): Количество необходимых семплов
-            трансформации.
-        """        
+        Parameters
+        ----------
+        base_transforms : Callable
+            Трансформации для изображений.
+        n_views : int, optional
+            Количество необходимых трансформированных семплов.
+            По умолчанию - 2.
+        """
         self.base_transforms = base_transforms
         self.n_views = n_views
         
     def __call__(self, x: torch.Tensor) -> List[torch.Tensor]:
+        """Произвести трансформацию изображения.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Исходное изображение.
+
+        Returns
+        -------
+        List[torch.Tensor]
+            Лист с разными обработками одного изображения.
         """
-        Произвести трансформацию изображения.
-
-        Args:
-            x (torch.Tensor): Исходное изображение.
-
-        Returns:
-            List[torch.Tensor]: Лист с разными обработками одного изображения.
-        """        
         return [self.base_transforms(x) for i in range(self.n_views)]
 
 
-class RegionGetting:  
-    """
-    Селектор регионов.
+class RegionGetting:
+    """Селектор регионов.
 
     Позволяет брать регионы с одного изображения без повторов и с минимальным
     заданным расстоянием между друг другом.
-    """    
+    """
     def __init__(
         self,
         image_size: Tuple[int, int],
@@ -52,20 +55,24 @@ class RegionGetting:
         regions_per_image: int = 2,
         region_margin: int = 1
     ):
-        """
-        Инициализация селектора.
+        """Инициализация селектора.
 
-        Args:
-            image_size (Tuple[int, int]): Размер изображений, из которых будут
-            браться регионы.
-            region_size (Tuple[int, int]): Размер регионов, которые будут
-            извлекаться.
-            stride (int, optional): Шаг окна региона.
-            regions_per_image (int, optional): Сколько регионов необходимо
-            выбрать с одного изображения.
-            region_margin (int, optional): Минимальное расстояние в регионах
-            между выбираемыми регионами.
-        """        
+        Parameters
+        ----------
+        image_size : Tuple[int, int]
+            Размер изображений, из которых будут браться регионы.
+        region_size : Tuple[int, int]
+            Размер регионов, которые будут извлекаться.
+        stride : int, optional
+            Шаг окна региона. По умолчанию `None`, что означает, что шаг будет
+            равен размеру окна.
+        regions_per_image : int, optional
+            Сколько регионов необходимо выбрать с одного изображения.
+            По умолчанию - 2.
+        region_margin : int, optional
+            Минимальное расстояние в регионах между выбираемыми регионами.
+            По умолчанию - 1.
+        """
         self.region_margin = region_margin
         self.regions_per_image = regions_per_image
         self.image_size = image_size
@@ -92,17 +99,20 @@ class RegionGetting:
         self.y_windows = self.y_indexer.size(0)
 
     def __call__(self, img: torch.Tensor) -> List[torch.Tensor]:
+        """Выбрать регионы из изображения или батча изображений.
+
+        Parameters
+        ----------
+        img : torch.Tensor
+            Изображение или батч.
+
+        Returns
+        -------
+        List[torch.Tensor]
+            Список тензоров с выбранными регионами размерами
+            `[c, h_reg, w_reg]` для одного изображения и `[b, c, h_reg, w_reg]`
+            для батча.
         """
-        Выбрать регионы из изображения или батча изображений.
-
-        Args:
-            img (torch.Tensor): Изображение или батч.
-
-        Returns:
-            List[torch.Tensor]: Список тензоров с выбранными регионами
-            размерами `[c, h_reg, w_reg]` для одного изображения и
-            `[b, c, h_reg, w_reg]` для батча.
-        """        
         h, w = self.image_size
         
         # Регионы будут браться исходя из логической матрицы
@@ -136,7 +146,7 @@ class RegionGetting:
 class RegionsDataset(torch.utils.data.Dataset):
     """
     Датасет возвращающий изображения регионов из указанной директории.
-    """    
+    """
 
     def __init__(
         self,
@@ -144,13 +154,15 @@ class RegionsDataset(torch.utils.data.Dataset):
         processing: Callable = None,
         **kwargs
     ):
-        """
-        Инициализация датасета.
+        """Инициализация датасета.
 
-        Args:
-            image_directory (Path): Директория с изображениями.
-            processing (Callable, optional): Функции предобработки данных.
-        """        
+        Parameters
+        ----------
+        image_directory : Path
+            Директория с изображениями.
+        processing : Callable, optional
+            Функции предобработки данных. По умолчанию - None.
+        """
         self.images = list(map(str, image_directory.rglob('*.jpg')))
         self.processing = processing
         super().__init__(**kwargs)
@@ -159,15 +171,18 @@ class RegionsDataset(torch.utils.data.Dataset):
         return len(self.images)
     
     def __getitem__(self, idx: int) -> torch.Tensor:
+        """Возвращает изображение из датасета по индексу.
+
+        Parameters
+        ----------
+        idx : int
+            Индекс изображения.
+
+        Returns
+        -------
+        torch.Tensor
+            Тензор со считанным изображением.
         """
-        Возвращает изображение из датасета по индексу.
-
-        Args:
-            idx (int): Индекс изображения.
-
-        Returns:
-            torch.Tensor: Тензор со считанным изображением.
-        """        
         img = cv2.imread(self.images[idx])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = torch.tensor(img).permute(2, 0, 1)
@@ -177,12 +192,13 @@ class RegionsDataset(torch.utils.data.Dataset):
         return img
     
     def calculate_mean_std(self) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Calculate mean and std for every channel over dataset.
+        """Взять среднее и std для каждого канала по всему датасету.
 
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: Mean and std of the dataset.
-        """        
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor]
+            Среднее и std каналов по датасету.
+        """
         means = []
         stds = []
         for i in range(len(self)):
